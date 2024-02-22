e9479a097ea8080a393216eb852c22774eaf3242
Giovanna Portal da Costa <giovanna.portaldacosta@gmail.com>
2024-02-21 21:41:11 -0300

    21.02.24

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,37 @@
 
-console.log('sucesso!');
+const offset = 0
+const limit = 10
+const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonToLi (pokemon){
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://www.serebii.net/swordshield/pokemon/001.png" 
+            alt=${pokemon.name}>
+        </div>
+        </li>
+        `
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonbody) => jsonbody.results)
+    .then((pokemons) => {
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i];
+            pokemonList.innerHTML += convertPokemonToLi(pokemon);
+        }
+    })
+    .catch((error) => console.error(error))
+    
+    
